69654453417384362ba0248eca42d8928d3fba5a
www <www@DESKTOP-HQHOP14>
2017-12-19 11:01:31 +0800

    杀杀杀

---
+++ src/main/java/com/wl/edu/wlroom/controller/Hello.java
@@ -0,0 +1,14 @@
+package com.wl.edu.wlroom.controller;
+
+public class Hello {
+	private String info;
+
+	public String getInfo() {
+		return info;
+	}
+
+	public void setInfo(String info) {
+		this.info = info;
+	}
+	
+}
