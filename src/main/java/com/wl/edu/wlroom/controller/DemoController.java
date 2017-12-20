ac960cbea8ef8a18047a355b037d3186b28cc492
windycba <34636849+windycba@users.noreply.github.com>
2017-12-20 17:15:31 +0800

    啊啊

---
+++ src/main/java/com/wl/edu/wlroom/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.wl.edu.wlroom.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/wl")
+public class DemoController {
+	@RequestMapping(value="/demo",method=RequestMethod.GET)
+	public String Demo(){
+		return "boom";
+	}
+
+}
