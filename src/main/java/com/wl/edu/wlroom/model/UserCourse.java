5641b5bf23d24fa544c399e35ba3145b6a7b3620
windycba <1655560412@qq.com>
2018-02-28 16:49:17 +0800

    1

---
+++ src/main/java/com/wl/edu/wlroom/model/UserCourse.java
@@ -0,0 +1,15 @@
+package com.wl.edu.wlroom.model;
+
+public class UserCourse extends User {
+    private String course;
+    public String getCourse() {
+        return course;
+    }
+
+    public void setCourse(String course) {
+        this.course = course;
+    }
+
+
+
+}
